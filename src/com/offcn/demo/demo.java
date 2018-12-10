66f4c01bc856ba07fb865d79ef6f7c8131e4c8ab
huan668hd <2218622053@qq.com>
2018-12-10 20:17:50 +0800

    第2次提交gitdemo项目

--- src/com/offcn/demo/demo.java
+++ src/com/offcn/demo/demo.java
@@ -4,4 +4,7 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("hellow world!!!");
     }
+    public void fun(){
+        System.out.println("11111");
+    }
 }
---
+++ src/com/offcn/demo/demo11.java
@@ -0,0 +1,7 @@
+package com.offcn.demo;
+
+public class demo11 {
+    public static void main(String[] args) {
+        System.out.println("122334");
+    }
+}
